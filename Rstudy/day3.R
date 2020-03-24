4b349b8433f815f9584592fd12d68095fcd47d37
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-03-24 13:52:02 +0900

    add 2020 - 03 - 24 (cat & switch & for문)

--- Rstudy/day3.R
+++ Rstudy/day3.R
@@ -81,6 +81,9 @@ summary(df2$V2)
 for(data in month.name) 
   print(data)
 
+for(data in month.name)
+  cat(data)
+
 sum <- 0
 for(i in 5:15){
   if(i%%10==0){
@@ -90,11 +93,21 @@ for(i in 5:15){
   print(paste(i,":",sum))
 }
 
+sum <- 0
+for(i in 5:15){
+  if(i%%10==0){
+    break
+  }
+  sum <- sum + i
+  cat(i,":",sum,"\n")
+}
+
 
 sum <-0
 for(i in 5:15){
   if(i%%10==0){
-    next;  #continue
+    next; 
+#R에서의 next는 Java나 JavaScript에서의 continue와 기능이 똑같다.
   }
   sum <- sum + i
   print(paste(i,":",sum))
@@ -119,6 +132,134 @@ repeat {
   cat(sumNumber,"\n")
   if(sumNumber > 20)
     break;
+}
+
+
+#제어문
+#if else
+randomNum <-sample(1:10,1)
+if(randomNum>5){
+  cat(randomNum,":5보다 크군요","\n")
+}else{
+  cat(randomNum,":5보다 작거나 같군요","\n")
+}
+
+if(randomNum%%2 == 1){
+  cat(randomNum,";홀수\n")
+}else{
+  cat(randomNum,";짝수","\n")
+}
+
+
+if(randomNum%%2 == 1){
+  cat(randomNum,";홀수","\n")
+}else{
+  cat(randomNum,";짝수","\n")
+}
+
+score <- sample(0:100, 1)  # 0~100 숫자 한 개를 무작위로 뽑아서
+if (score >=90){
+  cat(score,"는 A등급입니다","\n")
+}else if (score >=80){
+  cat(score,"는 B등급입니다","\n")
+}else if (score >=70){
+  cat(score,"는 C등급입니다","\n")
+}else if (score >=60){
+  cat(score,"는 D등급입니다","\n")
+}else {
+  cat(score,"는 F등급입니다","\n")
+}
+
+#for문
+#for 실습
+for(data in month.name) 
+  print(data)
+for(data in month.name)print(data);print("ㅋㅋ")
+for(data in month.name){print(data);print("ㅋㅋ")}
+
+for(n in 1:5)
+  cat("hello?","\n")
+
+for(i in 1:5){
+  for(j in 1:5){
+    cat("i=",i,"j=",j,"\n")
+  }
+}
+# 구구단
+for(dan in 1:9){
+  for(num in 1:9){
+    cat(dan,"x",num,"=",dan*num,"\t") # \n : 개행문자, \t : 탭문자
+  }
+  cat("\n")
+}
+
+
+bb <- F
+for(i in 1:9){
+  for(j in 1:9){
+    if(i*j>30){
+      bb<-T
+      break
+    } 
+    cat(i,"*",j,"=",i*j,"\t")
+  }
+  cat("\n")
+  if(bb) #bb가 TRUE이면
+    break
+}
+
+#while문
+i<-1
+while(i <= 10){
+  cat(i,"\n")
+  i <- i+1
+}
+cat("종료 후 :",i,"\n")
+
+i<-1
+while (i<=10) {
+  cat(i,"\n")
+}
+
+i<-1
+while (i<=10) {
+  cat(i,"\n")
+  i<-i+2
+}
+
+i<-1
+while (i<=10) {
+  cat(i,"\n")
+  i<-i+1
+}
+
+#switch 문을 대신하는 함수
+month <- sample(1:12,1)
+month <- paste(month,"월",sep="") # "3월"  "3 월"
+result <- switch(EXPR=month,
+                 "12월"=,"1월"=,"2월"="겨울",
+                 "3월"=,"4월"=,"5월"="봄",
+                 "6월"=,"7월"=,"8월"="여름",
+                 "가을")
+cat(month,"은 ",result,"입니다\n",sep="")
+
+
+
+num <- sample(1:10,1)
+num
+switch(EXPR = num,"A","B","C","D")
+# 1이면 A, 2 이면 B, 3 이면 C, 4 이면 D, 5~10 이면 아무것도 출력되지 않게된다.
+# 수치데이터의 경우에는 default 식(값)을 지정하지 못한다.
+
+for(num in 1:10){
+  cat(num,":",switch(EXPR = num,"A","B","C","D"),"\n")
+}
+
+for(num in 1:10){
+  num <- as.character(num) #as.character로 형변환을 하고있다.
+  #이렇게 캐릭터로 바꾸어주면 비교값을 줄 수 있게 된다.
+  cat(num,":",switch(EXPR = num,
+                     "7"="A","8"="B","9"="C","10"="D","ㅋ"),"\n")
 }
 
 
