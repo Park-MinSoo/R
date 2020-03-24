bdc0f9f3b6d4cc350fb4e48fbb0af97a17b4fb3e
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-03-24 16:43:29 +0900

    add 2020 - 03 - 24 (function)

--- Rstudy/day3.R
+++ Rstudy/day3.R
@@ -277,7 +277,7 @@ result
 xx  # 오류발생
 
 
-func2 <- function(x,y) {
+func2 <- function(x,y) { #fnc2도 객체이다. 할당된 값이 함수인 객체
   xx <- x
   yy <- y
   return(sum(xx, yy))
@@ -296,12 +296,75 @@ func3 <- function(x,y) {
 func3(9, 19)  # 30
 
 func4 <- function(x=100, y=200, z) {
+  #여기서 x 와 y 는 default 값이 있는 변수이다.
+  # x 와 y 는 선택적이지만 z는 반드시 값이 와야한다.
   return(x+y+z)
 }
 func4()
-func4(10,20,30)
+func4(10,20,30) # 기본값은 무시하게 된다.
 func4(x=1,y=2,z=3)
-func4(y=11,z=22,x=33)
+func4(y=11,z=22,x=33) # 이렇게 값에 할당해서 넣어줄 때에는 순서는 무시해도 된다.
 func4(z=1000)  
 
+# 쉬트에 있는 함수 코드
+f1 <- function() print("TEST")
+f1()
+r <- f1()
+r
+f2 <- function(num) {print("TEST"); print(num) }
+f2(100)
+f2()
+f3<- function (p="R") print(p)
+f3()
+f3(p="PYTHON")
+f3("java")
+f4<- function (p1="ㅋㅋㅋ",p2) for(i in 1:p2) print(p1)
+f4(p1="abc", p2=3)
+f4("abc", 3) 
+f4(5) 
+f4(p2=5) 
+f5<- function(...) { print("TEST"); data <- c(...); print(length(data))}
+f5(10, 20, 30)
+f5("abc", T, 10, 20)
+f6<- function(...) {
+  print("수행시작")
+  data <- c(...)
+  for(item in data) {
+    print(item)
+  }
+  return(length(data))
+}
+f6()
+f6(10)
+f6(10,20)
+f6(10,20,30)
+f6(10,'abc', T, F)
 
+f7<- function(...) {
+  data <- c(...)
+  sum <- 0;
+  for(item in data) {
+    if(is.numeric(item))
+      sum <- sum + item
+    else
+      print(item)
+  }
+return(sum)
+}
+f7(10,20,30)
+f7(10,20,'test', 30,40)
+
+f8<- function(...) {
+  data <- list(...)
+  sum <- 0;
+  for(item in data) {
+    if(is.numeric(item))
+      sum <- sum + item
+    else
+      print(item)
+  }
+  return(sum)
+}
+
+f8(10,20,30)
+f8(10,20,"test", 30,40)
