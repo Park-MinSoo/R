6f9b7b07c7357bfa1927dd54bf9b2e9f06323a9f
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-04-07 10:46:01 +0900

    add 2020 - 04 - 07

---
+++ Rstudy/day7.R
@@ -0,0 +1,139 @@
+# 링크 클릭으로 AJAX 로 처리되는 네이버 웹툰 댓글 읽어 오기
+library(RSelenium)
+repl_v = NULL;
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+url<-'http://comic.naver.com/comment/comment.nhn?titleId=570503&no=135'
+remDr$open()
+remDr$navigate(url)
+#베스트 댓글 내용 읽어오기
+doms1<-remDr$findElements(using ="css","ul.u_cbox_list span.u_cbox_contents")
+vest_repl <- sapply(doms1,function(x){x$getElementText()}) 
+#호출 될 때 마다 x에 담는다., sapply의 s는 simple의 뜻이므로 간단한것을 담는다.
+#여기서는 getElementText()를 호출하는 것이므로 컨텐트를 뽑아와 추출하게 된다.
+repl_v <- c(repl_v, unlist(vest_repl))
+
+toralReview <- remDr$findElement(using='css','span.u_cbox_in_view_comment')
+toralReview$clickElement()
+#전체 댓글의 첫 페이지 내용 읽어오기
+doms2<-remDr$findElements(using ="css selector","ul.u_cbox_list span.u_cbox_contents")
+repl <-sapply(doms2,function(x){x$getElementText()})
+repl_v <- c(repl_v, unlist(repl))
+
+repeat {
+  for (i in 4:12) {               
+    nextCss <- paste("#cbox_module>div>div.u_cbox_paginate>div> a:nth-child(",i,") > span", sep="")                
+    try(nextListLink<-remDr$findElement(using='css',nextCss))
+    if(length(nextListLink) == 0)   break;
+    nextListLink$clickElement() #페이지 이동을 한다는 뜻, 즉 브라우저가 렌더링을 한다는 뜻.
+    Sys.sleep(1)# 즉, 브라우저가 일할 시간을 줘야한다.
+    #전체 댓글의 해당 페이지 내용 읽어오기
+    doms3<-remDr$findElements(using ="css selector","ul.u_cbox_list span.u_cbox_contents")
+    repl <-sapply(doms3,function(x){x$getElementText()})
+    repl_v <- c(repl_v, unlist(repl))                
+  }
+  
+  try(nextPage<-remDr$findElement(using='css',
+                                  "#cbox_module > div > div.u_cbox_paginate > div > a:nth-child(13) > span.u_cbox_cnt_page"))
+  if(length(nextPage) == 0)  break; #맨 끝페이지 까지 가게되면 무한루프를 끝내라.
+  nextPage$clickElement()
+  Sys.sleep(1)
+  doms2<-remDr$findElements(using ="css selector","ul.u_cbox_list span.u_cbox_contents")
+  repl <-sapply(doms2,function(x){x$getElementText()})
+  repl_v <- c(repl_v, unlist(repl))        
+}
+print(repl_v)
+write(repl_v, "webtoon2.txt")
+
+
+# [ 신라스테이 호텔에 대한 전체 페이지 댓글 읽기 ]
+remDr <- remoteDriver(remoteServerAddr = "localhost" , port = 4445, browserName = "chrome")
+remDr$open()
+url<-'https://www.agoda.com/ko-kr/shilla-stay-yeoksam/hotel/seoul-kr.html?asq=z91SVm7Yvc0eRE%2FTBXmZWCYGcVeTALbG%2FvMXOYFqqcm2JknkW25Du%2BVdjH%2FesXg8ORIaVs1PaEgwePlsVWfwf3sX%2BVNABRcMMOWSvzQ9BxqOPOsvzl8390%2BEhEylPvEiBp0eoREr2xLYHgqmk0Io4J1HYEzEOqyvdox%2BwS6yxHeonB9lh7mJsBIjSBPoMzBLFW01k%2BU8s2bGO6PcSdsu3T30HwabyNzwNYKiv%2BRDxfs%3D&hotel=699258&tick=637215342272&languageId=9&userId=bcb7ecc6-7719-465f-bf29-951e39733c66&sessionId=uouhnqjisace4freagmzbxxc&pageTypeId=7&origin=KR&locale=ko-KR&cid=-1&aid=130243&currencyCode=KRW&htmlLanguage=ko-kr&cultureInfoName=ko-KR&ckuid=bcb7ecc6-7719-465f-bf29-951e39733c66&prid=0&checkIn=2020-05-30&checkOut=2020-05-31&rooms=1&adults=1&childs=0&priceCur=KRW&los=1&textToSearch=%EC%8B%A0%EB%9D%BC%EC%8A%A4%ED%85%8C%EC%9D%B4%20%EC%97%AD%EC%82%BC%20(Shilla%20Stay%20Yeoksam)&productType=-1&travellerType=0&familyMode=off'
+remDr$navigate(url)
+Sys.sleep(3)
+pageLink <- NULL
+reple <- NULL
+curr_PageOldNum <- 0
+repeat{
+  doms <- remDr$findElements(using = "css selector", ".Review-comment-bodyText")
+  Sys.sleep(1)
+  reple_v <- sapply(doms, function (x) {x$getElementText()})
+  print(reple_v)
+  reple <- append(reple, unlist(reple_v))
+  cat(length(reple), "\n")
+  pageLink <- remDr$findElements(using='css',"#reviewSection > div:nth-child(6) > div > span:nth-child(3) > i ")
+  remDr$executeScript("arguments[0].click();",pageLink)
+  Sys.sleep(2)
+  curr_PageElem <- remDr$findElement(using='css','#reviewSection > div:nth-child(6) > div > span.Review-paginator-numbers > span.Review-paginator-number.Review-paginator-number--current')
+  curr_PageNewNum <- as.numeric(curr_PageElem$getElementText())
+  cat(paste(curr_PageOldNum, ':', curr_PageNewNum,'\n')) 
+# 방대한 양의 웹데이터를 크롤링 중  페이지 전환시 중간에 에러가 난다 하더라도 어디서 났는지 확인 할 수 있게 하기 위해 책갈피 같이 넣어놓는 것이다.
+  if(curr_PageNewNum == curr_PageOldNum)  {
+    cat("종료\n")
+    break; 
+  }
+  curr_PageOldNum <- curr_PageNewNum;
+}
+cat(length(reple), "개의 댓글 추출\n")
+write(reple,"hotel.txt")
+
+# [ YES24의 명견만리 댓글 읽어오기 ]
+
+library(RSelenium)
+remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445, browserName = "chrome")
+remDr$open()
+remDr$navigate("http://www.yes24.com/24/goods/40936880")
+
+
+webElem <- remDr$findElement("css", "body")
+remDr$executeScript("scrollTo(0, 0)", args = list(webElem))
+Sys.sleep(1)
+remDr$executeScript("scrollBy(0, 3200)", args = list(webElem))
+Sys.sleep(1)
+remDr$executeScript("scrollBy(0, 3200)", args = list(webElem))
+Sys.sleep(1)
+remDr$executeScript("scrollBy(0, 3200)", args = list(webElem))
+Sys.sleep(3)
+repl_v = NULL
+endFlag <- FALSE
+page <- 3
+
+repeat {
+  for(index in 3:7) {
+    fullContentLinkCSS <- paste("#infoset_reviewContentList > div:nth-child(",index,") > div.reviewInfoBot.crop > a", sep='')
+    fullContentLink<-remDr$findElements(using='css selector',  fullContentLinkCSS)
+    if (length(fullContentLink) == 0) {
+      cat("종료\n")
+      endFlag <- TRUE
+      break
+    }
+    remDr$executeScript("arguments[0].click();",fullContentLink);
+    Sys.sleep(1)
+    fullContentCSS <- paste("#infoset_reviewContentList > div:nth-child(",index,") > div.reviewInfoBot.origin > div.review_cont > p", sep='')
+    fullContent<-remDr$findElements(using='css selector', fullContentCSS)
+    print(fullContent)
+    repl <-sapply(fullContent,function(x){x$getElementText()})    
+    print(repl)
+    repl_v <- c(repl_v, unlist(repl))
+  }
+  if(endFlag)
+    break;  
+  
+  if(page == 10){
+    page <- 3
+    nextPageCSS <- "#infoset_reviewContentList > div.review_sort.sortTop > div.review_sortLft > div > a.bgYUI.next"
+  }
+  else{
+    page <- page+1;
+    nextPageCSS <- paste("#infoset_reviewContentList > div.review_sort.sortBot > div.review_sortLft > div > a:nth-child(",page,")",sep="")
+  }
+  #infoset_reviewContentList > div.review_sort.sortBot > div.review_sortLft > div > a:nth-child(4)
+  #infoset_reviewContentList > div.review_sort.sortBot > div.review_sortLft > div > a:nth-child(5)
+  remDr$executeScript("scrollTo(0, 0)", args = list(webElem))
+  nextPageLink<-remDr$findElements(using='css selector',nextPageCSS) 
+  remDr$executeScript("arguments[0].click();",nextPageLink);
+  #sapply(nextPageLink,function(x){x$clickElement()})  
+  Sys.sleep(5)
+  print(page)
+}
+write(repl_v, "yes24.txt")
