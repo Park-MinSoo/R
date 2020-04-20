6c437a938612536eaf8dc0c37e998920c7f86777
Park Min Soo <58577697+Park-MinSoo@users.noreply.github.com>
2020-04-20 13:43:23 +0900

    add 2020 - 04 - 20 (1인가구 분석 출력)

--- Rstudy/oneMap.R
+++ Rstudy/oneMap.R
@@ -1,24 +1,36 @@
+install.packages("rgeos")
 library(dplyr)
 library(Kormaps)
 library(leaflet)
 
 DONG<-read.csv('data/one.csv')
-View(DONG)
+#View(DONG)
 
 k3 <- korpopmap3
 k3@data[c("C행정구역별_읍면동", "행정구역별_읍면동")]
 View(k3@data)
 
+guname <- '강남구'
+gucode <- k2@data[k2@data$name == guname, "code.data"]
+pattern <- paste0('^', gucode)
+ccco <- grep(pattern, k3@data$code.data) # 변수를 따로 담아주어야한다.
+k3@data[ccco,]
+
+
 # 강남구 1인 가구 뽑기
+k3@polygons<-k3@polygons[ccco]
+
 k3@data$name<-gsub('·','',k3@data$name) # 동목록 에서 . 을 빼서 다시 담음
-View(k3@data$name)
+#View(k3@data$name)
 
 colnames(DONG)<-c('구별','name','일인가구') # DONG 칼럼명 재설정
-View(DONG)
+#View(DONG)
 
 dong <- DONG %>%filter(구별=='강남구')
+dong
 
 k3@data<-merge(k3@data,dong,by.x='name',sort=FALSE)
+View(k3@data)
 mymap <- k3@data
 
 mypalette <- colorNumeric(palette ='Set3' , domain = k3@data$'일인가구')
@@ -37,7 +49,6 @@ map7<-leaflet(k3) %>%
              title = '인구수',
              opacity = 1)
 map7	
-View(map7)
-
+library(htmlwidgets)
 saveWidget(map7, file="oneMap.html")
 
